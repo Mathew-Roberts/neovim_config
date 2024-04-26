ddbbb417aa0df2cdc37bad4d682c3a19378163a2
mat roberts <mathewjamesroberts99@gmail.com>
2024-04-26 23:44:33 +0100

    adding some linting

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -1,10 +1,11 @@
 return {
 	"stevearc/conform.nvim",
 	config = function()
-		require("conform").setup({
+		local conform = require("conform")
+		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				python = { "ruff_format" },
+				python = { "ruff_fix", "ruff_format" },
 			},
 			format_on_save = {
 				-- These options will be passed to conform.format()
@@ -12,5 +13,12 @@ return {
 				lsp_fallback = true,
 			},
 		})
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,8 +1,14 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		dependencies = {
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
 		config = function()
 			require("mason").setup({})
+			require("mason-tool-installer").setup({
+				ensure_installed = { "mypy", "ruff", "stylua" },
+			})
 		end,
 	},
 	{
