c945e14cff0843a616ec92c15ba66278579e388b
Mat Roberts <mat.roberts@quantile.com>
2024-05-11 20:49:21 +0000

    changes

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,6 +4,9 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup()
+
+		local oil = require("oil")
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<leader>sh", oil.toggle_hidden, { desc = "Open parent directory" })
 	end,
 }
