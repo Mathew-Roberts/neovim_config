23c67938491d46ea1504b8fead64d4cd1e0a869d
mat roberts <mathewjamesroberts99@gmail.com>
2024-05-25 22:31:26 +0100

    Adding neogit.lua file with very basic git integrations

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+	"NeogitOrg/neogit",
+	tag = "v0.0.1",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+
+		-- Only one of these is needed, not both.
+		"nvim-telescope/telescope.nvim", -- optional
+	},
+	config = true,
+}
