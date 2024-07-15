6e938c00d009693cd6c30449051c88244ad76449
Mat Roberts <mat.roberts@quantile.com>
2024-07-15 13:27:26 +0000

    adding git conflict

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -91,4 +91,13 @@ return {
       }
     end,
   },
+  {
+    'akinsho/git-conflict.nvim',
+    version = 'v2.0.0',
+    config = function()
+      require('git-conflict').setup {
+        disable_diagnostics = true,
+      }
+    end,
+  },
 }
