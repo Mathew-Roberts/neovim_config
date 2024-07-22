4666d0809f40792b6bc26ee5cfc85451e651fb2c
Mat Roberts <mat.roberts@quantile.com>
2024-07-22 09:40:57 +0000

    adding vim tmux navigator

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -114,4 +114,21 @@ return {
       vim.keymap.set('n', '<leader>o', ':DogeGenerate<CR>', { noremap = true, silent = true })
     end,
   },
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
 }
